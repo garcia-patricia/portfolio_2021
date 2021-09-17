00d1bfc9d1cf949aa2faec4ecbebc38ca2ecbc2d
Trisha Garcia <garcia-patricia@live.com>
2021-09-17 09:23:00 -0400

    ore intro edits

--- sections/intro.php
+++ sections/intro.php
@@ -1,7 +1,7 @@
 <section class="intro">
     <div class="intro-content">
 <!-- intro content begins -->
-<div class="intro-pic">
+<div class="intro-pic ">
         <img src="img/celladore_thumb.jpg" alt="Hi! I'm Patricia Morales. A graphic designer, illustrator and aspiring front-end web developer located in London Ontario!">
     </div>
 
@@ -13,6 +13,7 @@
         <p>
         Morbi purus massa, lobortis at lectus in, consequat elementum elit. Maecenas eget metus bibendum, tristique justo eu, fermentum neque. Cras leo metus, vehicula eget orci non, eleifend mollis massa. Nunc imperdiet odio nec libero varius porta. Vivamus magna est, commodo eget lacus et, maximus luctus est. 
         </p>
+        <div class="btn-about">about</div>
     </div>
 <!-- intro content ends -->
     </div>
